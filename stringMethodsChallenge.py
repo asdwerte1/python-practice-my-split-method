8f2804f7b8caf4e2e2889daf3c2f35f44677bd80
asdwerte1 <samjmarkham@outlook.com>
2023-10-20 14:18:47 +0100

    Initial State - template for challenge

---
+++ stringMethodsChallenge.py
@@ -0,0 +1,10 @@
+def mysplit(strng):
+    #
+    #Code here
+    #
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
