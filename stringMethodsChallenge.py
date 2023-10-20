b8bc5e0fb1cbad55fe0b06d429731b60e5c3e697
asdwerte1 <samjmarkham@outlook.com>
2023-10-20 14:20:53 +0100

    Added task description to start of program as comment

--- stringMethodsChallenge.py
+++ stringMethodsChallenge.py
@@ -1,3 +1,8 @@
+#Task - write a funciton that behaves like the split method
+#Function should return a list of words created from an input string
+#The list should divide the words whenever there is a space
+#If the string is empty -the function should return an empty list
+
 def mysplit(strng):
     #
     #Code here
